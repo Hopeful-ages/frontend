501f926d8deb5f67f4928a34f18c36158be62afb
pedrodalagnol <joao.rossatti@edu.pucrs.br>
2025-09-01 19:04:55 -0300

    Create Login.tsx

---
+++ src/app/pages/Login.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    console.log('Email:', email);
+    console.log('Senha:', password);
+    alert(`Tentativa de login com o email: ${email}`);
+  };
+
+  return (
+    <div className="login-form-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Senha:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
