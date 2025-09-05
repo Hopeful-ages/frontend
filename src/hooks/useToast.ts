1dce34bfa9ade1aed1f8d52f3eeb7adfdf311d02
Thiago Goulart <thiago.goulart001@edu.pucrs.br>
2025-09-04 23:55:34 -0300

    fixing eslint error

--- src/hooks/useToast.ts
+++ src/hooks/useToast.ts
@@ -68,7 +68,7 @@ const addToRemoveQueue = (toastId: string, duration = TOAST_REMOVE_DELAY) => {
   const timeout = setTimeout(() => {
     toastTimeouts.delete(toastId);
     dispatch({
-      type: 'REMOVE_TOAST',
+      type: actionTypes.REMOVE_TOAST,
       toastId: toastId,
     });
   }, duration);
@@ -78,13 +78,13 @@ const addToRemoveQueue = (toastId: string, duration = TOAST_REMOVE_DELAY) => {
 
 export const reducer = (state: State, action: Action): State => {
   switch (action.type) {
-    case 'ADD_TOAST':
+    case actionTypes.ADD_TOAST:
       return {
         ...state,
         toasts: [action.toast, ...state.toasts].slice(0, TOAST_LIMIT),
       };
 
-    case 'UPDATE_TOAST':
+    case actionTypes.UPDATE_TOAST:
       return {
         ...state,
         toasts: state.toasts.map((t) =>
@@ -92,7 +92,7 @@ export const reducer = (state: State, action: Action): State => {
         ),
       };
 
-    case 'DISMISS_TOAST': {
+    case actionTypes.DISMISS_TOAST: {
       const { toastId } = action;
 
       if (toastId) {
@@ -115,7 +115,7 @@ export const reducer = (state: State, action: Action): State => {
         ),
       };
     }
-    case 'REMOVE_TOAST':
+    case actionTypes.REMOVE_TOAST:
       if (action.toastId === undefined) {
         return {
           ...state,
@@ -147,13 +147,14 @@ function toast({ duration = TOAST_REMOVE_DELAY, ...props }: Toast) {
 
   const update = (props: ToasterToast) =>
     dispatch({
-      type: 'UPDATE_TOAST',
+      type: actionTypes.UPDATE_TOAST,
       toast: { ...props, id },
     });
-  const dismiss = () => dispatch({ type: 'DISMISS_TOAST', toastId: id });
+  const dismiss = () =>
+    dispatch({ type: actionTypes.DISMISS_TOAST, toastId: id });
 
   dispatch({
-    type: 'ADD_TOAST',
+    type: actionTypes.ADD_TOAST,
     toast: {
       ...props,
       id,
@@ -234,7 +235,8 @@ function useToast() {
     error,
     warning,
     info,
-    dismiss: (toastId?: string) => dispatch({ type: 'DISMISS_TOAST', toastId }),
+    dismiss: (toastId?: string) =>
+      dispatch({ type: actionTypes.DISMISS_TOAST, toastId }),
   };
 }
 
