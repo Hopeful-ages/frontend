711dbcbf041e0eb4fc82c8bec0d5c23af790ba30
João Vitor Krampe Schwening <joaovitorkrampe@gmail.com>
2025-10-20 23:35:17 -0300

    development planSearchPage and implemented useEffect for the call api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         <LoadingProvider>
-          <HeaderWrapper />
+          {/* <HeaderWrapper /> */}
           <Toaster />
           {children}
         </LoadingProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,65 @@
 'use client';
 
-import Header from '@/components/Header';
+import { useState, useEffect } from 'react';
+import { Dropdown } from '@/components/Dropdown';
+// import Header from '@/components/Header';
+import { CobradeDTO, CityResponseDTO } from '@/lib/types';
+// import { toastError } from '@/utils/toastError';
 
-export default function PlanSearch() {
+import { api } from '@/lib/api';
+
+export default function PlanSearchPage() {
+  const [cities, setCities] = useState<CityResponseDTO[]>([]);
+  const [city, setCity] = useState<CityResponseDTO | null>(null);
+
+  const [cobrade, setCobrade] = useState<CobradeDTO | null>(null);
+  const [cobrades, setCobrades] = useState<CobradeDTO[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const [citiesData, cobradesData] = await Promise.all([
+          api.getAllCities(),
+          api.getAllCobrades(),
+        ]);
+        setCities(citiesData);
+        setCobrades(cobradesData);
+      } catch (err) {
+        console.error(err);
+        // toastError('Erro ao carregar listas', 'Cidades ou COBRADE');
+      }
+    })();
+  }, []);
+
+  const handleSave = async () => {
+    if (!cobrade) {
+      // warning('Por favor, selecione um COBRADE.');
+      return;
+    }
+    if (!city) {
+      // warning('Por favor, selecione uma cidade.');
+      return;
+    }
+  };
+
   return (
     <main>
-      <Header />
+      {/* <Header /> */}
       <div>
-        <h1>TELA DE PESQUISA DE PLANOS AQUI</h1>
+        <h1>Planos de Contingência </h1>
+        <Dropdown
+          label="Selecione uma Cidade"
+          items={cities.map((c) => `${c.name} - ${c.state}`)}
+          size="large"
+          fullWidth
+          value={city ? `${city.name} - ${city.state}` : null}
+          onSelect={(cityString) => {
+            const cityName = cityString.split(' - ')[0];
+            const selectedCity =
+              cities.find((c) => c.name === cityName) || null;
+            setCity(selectedCity);
+          }}
+        />
       </div>
     </main>
   );
