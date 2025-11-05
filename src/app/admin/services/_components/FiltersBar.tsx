2a2b06f9de608d93844218e4a0108fadcaf04e5d
Andre Fabiao <andre.fabiao@outlook.com>
2025-11-05 09:02:33 -0300

    add confirm delete modal to services

--- src/app/admin/services/_components/FiltersBar.tsx
+++ src/app/admin/services/_components/FiltersBar.tsx
@@ -6,23 +6,21 @@ type FiltersBarProps = {
   onCreateAction: () => void;
 };
 
-export function FiltersBar({
-  onCreateAction,
-}: FiltersBarProps) {
+export function FiltersBar({ onCreateAction }: FiltersBarProps) {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="mb-4 sm:mb-6 ml-0 sm:ml-5 flex items-end justify-between px-4 sm:px-0">
+      <div className="mb-6 ml-5 flex items-end justify-start">
         <Button
           onClick={onCreateAction}
-          title="Adicionar serviço"
-          className="mt-4 sm:mt-8 mr-0 sm:mr-5 ml-auto items-center border border-black bg-white px-4 sm:px-8 py-1.5 sm:py-1 text-sm sm:text-base hover:bg-gray-100"
           variant="outline"
+          size={'sm'}
+          leftIcon={<Plus className="h-5 w-5" />}
         >
-          <Plus className="h-4 w-4 sm:h-5 sm:w-5 align-middle" />
+          Adicionar Serviço
         </Button>
       </div>
     </form>
